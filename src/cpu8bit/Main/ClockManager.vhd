1c8e0f19fa308f5519dccce08bd40e3550b23104
David <37296920+dma-neves@users.noreply.github.com>
2021-02-03 16:42:56 +0000

    Add files via upload

--- src/cpu8bit/Main/ClockManager.vhd
+++ src/cpu8bit/Main/ClockManager.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ClockManager is
 Port(
-	clk, reset : in STD_LOGIC;
+	clk : in STD_LOGIC;
 	run, step : in STD_LOGIC;
 	
 	clk_out : out STD_LOGIC
@@ -13,21 +13,9 @@ end ClockManager;
 
 architecture Behavioral of ClockManager is
 
-
-component DFlipFlop is
-Port(
-	D, En, R : in STD_LOGIC;
-	Q : OUT STD_LOGIC
-);
-end component;
-
-signal run_q : STD_LOGIC;
-
 begin
 
-RUN_DFF: DFlipFlop port map('1', run, reset, run_q);
-
-clk_out <= (clk and run_q) or step;
+clk_out <= step or (run and clk);
 
 end Behavioral;
 
