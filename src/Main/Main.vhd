cda168261ad40bddec01ff12bd38a198f1b97520
dma-neves <david.m.neves@live.com>
2021-12-08 18:44:43 +0000

    Revert "Updated Main/CPU IO"
    This reverts commit 1d004692ebd759a608d25db944d1c788a0d5642b.

--- src/Main/Main.vhd
+++ src/Main/Main.vhd
@@ -6,9 +6,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Main is
 Port(
 	clk : in STD_LOGIC;
-	reset, start, run, step : in STD_LOGIC;
-	
-	regA : out STD_LOGIC_VECTOR(7 downto 0)
+	reset, run, step : in STD_LOGIC;
+	regSel : in STD_LOGIC_VECTOR(2 downto 0);
+
+	GPR, regIC, regIR, regIDR, regIACR, regPACR, regADR : out STD_LOGIC_VECTOR(15 downto 0);
+	state_0, state_1, state_2, state_3, state_4, state_5, state_6 : out STD_LOGIC
 );
 end Main;
 
@@ -18,16 +20,19 @@ architecture Behavioral of Main is
 
 component CPU is
 Port(
-	reset, start : in STD_LOGIC;
+	reset : in STD_LOGIC;
 	clk : in STD_LOGIC;
-	dataIn : in STD_LOGIC_VECTOR(7 downto 0);
+	dataIn : in STD_LOGIC_VECTOR(15 downto 0);
+
+	regSel : in STD_LOGIC_VECTOR(2 downto 0);
 	
-	address : out STD_LOGIC_VECTOR(7 downto 0);
-	dataOut : out STD_LOGIC_VECTOR(7 downto 0);
+	address : out STD_LOGIC_VECTOR(15 downto 0);
+	dataOut : out STD_LOGIC_VECTOR(15 downto 0);
 	readWrite : out STD_LOGIC;
 	fetch : out STD_LOGIC; 
-	
-	regA : out STD_LOGIC_VECTOR(7 downto 0)
+
+	GPR, regIC, regIR, regIDR, regIACR, regPACR, regADR : out STD_LOGIC_VECTOR(15 downto 0);
+	state_0, state_1, state_2, state_3, state_4, state_5, state_6 : out STD_LOGIC
 );
 end component;
 
@@ -40,30 +45,30 @@ Port(
 );
 end component;
 
-component RAM32 is
+component RAM64 is
 Port(
-	adr : in STD_LOGIC_VECTOR(4 downto 0);
+	adr : in STD_LOGIC_VECTOR(5 downto 0);
 	En : in STD_LOGIC;
 	R : in STD_LOGIC;
 	rw : in STD_LOGIC;
-	DI : in STD_LOGIC_VECTOR(7 downto 0);
-	DO : out STD_LOGIC_VECTOR(7 downto 0)
+	DI : in STD_LOGIC_VECTOR(15 downto 0);
+	DO : out STD_LOGIC_VECTOR(15 downto 0)
 );
 end component;
 
-component ROM256 is
+component ROM is
 Port(
-	adr : in STD_LOGIC_VECTOR(7 downto 0);
+	adr : in STD_LOGIC_VECTOR(15 downto 0);
 	En : in STD_LOGIC;
-	DO : out STD_LOGIC_VECTOR(7 downto 0)
+	DO : out STD_LOGIC_VECTOR(15 downto 0)
 );
 end component;
 
 -- Signals
 signal clk_aux : STD_LOGIC;
-signal adr_aux, ramData, romData, dataToCPU, dataToMem : STD_LOGIC_VECTOR(7 downto 0);
-signal adr_mem : STD_LOGIC_VECTOR(4 downto 0);
-signal RW_aux : STD_LOGIC;
+signal adr, ramData, romData, dataToCPU, dataToMem : STD_LOGIC_VECTOR(15 downto 0);
+signal adr_mem : STD_LOGIC_VECTOR(5 downto 0);
+signal rw : STD_LOGIC;
 
 signal fetch, ramEn, romEn : STD_LOGIC; 
 
@@ -80,20 +85,36 @@ dataToCPU(4) <= ramData(4) or romData(4);
 dataToCPU(5) <= ramData(5) or romData(5);
 dataToCPU(6) <= ramData(6) or romData(6);
 dataToCPU(7) <= ramData(7) or romData(7);
+dataToCPU(8) <= ramData(8) or romData(8);
+dataToCPU(9) <= ramData(9) or romData(9);
+dataToCPU(10) <= ramData(10) or romData(10);
+dataToCPU(11) <= ramData(11) or romData(11);
+dataToCPU(12) <= ramData(12) or romData(12);
+dataToCPU(13) <= ramData(13) or romData(13);
+dataToCPU(14) <= ramData(14) or romData(14);
+dataToCPU(15) <= ramData(15) or romData(15);
 
-adr_mem(0) <= adr_aux(0);
-adr_mem(1) <= adr_aux(1);
-adr_mem(2) <= adr_aux(2);
-adr_mem(3) <= adr_aux(3);
-adr_mem(4) <= adr_aux(4);
+adr_mem(0) <= adr(0);
+adr_mem(1) <= adr(1);
+adr_mem(2) <= adr(2);
+adr_mem(3) <= adr(3);
+adr_mem(4) <= adr(4);
+adr_mem(5) <= adr(5);
 
 -- Port maps
 
 CLKM: ClockManager port map(clk, run, step, clk_aux);
 
-CPU_M: CPU port map(reset, start, clk_aux, dataToCPU, adr_aux, dataToMem, RW_aux, fetch, regA);
-RAM32_M: RAM32 port map(adr_mem, ramEn, reset, RW_aux, dataToMem, ramData);
-ROM256_M: ROM256 port map(adr_aux, romEn, romData);
+CPU_M: CPU port map(
+
+	reset, clk_aux, dataToCPU, regSel, adr, dataToMem, 
+	rw, fetch, GPR, regIC, regIR, regIDR, regIACR, 
+	regPACR, regADR, 
+	state_0, state_1, state_2, state_3, state_4, state_5, state_6
+);
+
+RAM64_M: RAM64 port map(adr_mem, ramEn, reset, rw, dataToMem, ramData);
+ROM_M: ROM port map(adr, romEn, romData);
 
 end Behavioral;
 
